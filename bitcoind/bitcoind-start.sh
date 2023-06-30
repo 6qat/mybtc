4161e69f74a8c9412094865962ce0dfe384b79fb
Guilherme Moreira <ceschiatti@gmail.com>
2023-06-30 02:29:22 +0200

    Update for new server

--- bitcoind/bitcoind-start.sh
+++ bitcoind/bitcoind-start.sh
@@ -2,7 +2,7 @@
 
 # Just a simple wrapper to start bitcoind.
 #
-# If using systemd, simply create a file (e.g. /etc/systemd/system/bitcoind.service) 
+# If using systemd, simply create a file (e.g. /etc/systemd/system/bitcoind.service)
 # from example file below and add this script in ExecStart.
 # https://raw.githubusercontent.com-/bitcoin/bitcoin/76deb30550b2492f9c8d9f0302da32025166e0c5/contrib/init/bitcoind.service
 #
@@ -20,11 +20,12 @@
 set -e
 
 # Let's wait for 30 seconds in case other processes need to come up first.
-sleep 2 
+sleep 2
 
 echo "Starting bitcoind..."
 
-/opt/bitcoin/bin/bitcoind --daemon --server -port=8333 -pid=/mnt/blockchain/btc-data/bitcoind.pid -datadir=/mnt/blockchain/btc-data/ -conf=/mnt/blockchain/btc-data/bitcoin.conf #-connect=10.8.0.3:8333 -connect=54.39.22.116:8333  -connect=187.55.40.86:8333 -connect=179.235.204.165:8333 -connect=186.218.58.150:8333
+/mnt/disco/app/bitcoin/bin/bitcoind --daemon --server -pid=/mnt/disco/data/bitcoin/bitcoind.pid -datadir=/mnt/disco/data/bitcoin -debuglogfile=/mnt/disco/log/bitcoin.log -txindex=1 #-conf=/mnt/blockchain/btc-data/bitcoin.conf #-connect=10.8.0.3:8333 -connect=54.39.22.116:8333  -connect=187.55.40.86:8333 -connect=179.235.204.165:8333 -connect=186.218.58.150:8333
 
 echo "Done!"
+
 
